dd2307492ff095b4c08b78693bdf60841aef2392
norristown <jarvasity@gmail.com>
2024-05-08 00:31:19 -0400

    can send friend requests from backend

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 usersRouter.post('/', async (request, response, next) => {
@@ -23,6 +24,37 @@ usersRouter.post('/', async (request, response, next) => {
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({});
   response.json(users);
+});
+
+// send friend request
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
+usersRouter.post('/:id/friend_request', async (request, response, next) => {
+  try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' });
+    }
+    // get logged user
+    const loggedUser = await User.findById(decodedToken.id);
+
+    // get user to send friend request to
+    const user = await User.findById(request.params.id);
+    const friendRequest = {
+      from: loggedUser,
+      status: 'pending',
+    };
+    user.friendRequests = user.friendRequests.concat(friendRequest);
+    const newUser = await user.save();
+    response.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
--- models/user.js
+++ models/user.js
@@ -10,11 +10,23 @@ const userSchema = new mongoose.Schema({
   chats: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chat' }],
   date: { type: Date, default: Date.now },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  friendRequests: [
+    {
+      from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      status: { type: String, enum: ['pending', 'accepted', 'rejected'] },
+      created: { type: Date, default: Date.now },
+    },
+  ],
 });
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id;
+    returnedObject.friendRequests.forEach((request) => {
+      request.id = request._id;
+      delete request._id;
+    });
+    delete returnedObject.friendRequests._id;
     delete returnedObject._id;
     delete returnedObject.__v;
     delete returnedObject.passwordHash;
