56714071cba934733914cf233e3fbfa6e8deee14
norristown <jarvasity@gmail.com>
2024-05-11 19:22:53 -0400

    controller for accepting friend request

--- controllers/users.js
+++ controllers/users.js
@@ -21,9 +21,23 @@ usersRouter.post('/', async (request, response, next) => {
   }
 });
 
+// get all users
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('chats').populate('friends');
+
   response.json(users);
+});
+
+// get logged user
+usersRouter.get('/:id', async (request, response, next) => {
+  try {
+    const user = await User.findById(request.params.id)
+      .populate('friends')
+      .populate({ path: 'chats', populate: { path: 'messages' } });
+    response.json(user);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // send friend request
@@ -51,6 +65,37 @@ usersRouter.post('/:id/friend_request', async (request, response, next) => {
     };
     user.friendRequests = user.friendRequests.concat(friendRequest);
     const newUser = await user.save();
+    response.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// accept friend request
+usersRouter.post('/:id/accept_friend_request', async (request, response, next) => {
+  try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' });
+    }
+
+    // logged user
+    const user = await User.findById(decodedToken.id);
+    // user who sent friend request
+    const from = await User.findById(request.params.id);
+    const newFriendRequests = user.friendRequests.filter((r) => r.from.toString() !== from.id);
+    const accepted = {
+      from: from.id,
+      status: 'accepted',
+    };
+    if (user.friends.find((f) => f.toString() === from.id)) {
+      return response.status(401).json({ error: `already friends with ${from.username}` });
+    }
+    user.friendRequests = [...newFriendRequests, accepted];
+    user.friends = [...user.friends, from];
+
+    const newUser = await user.save();
+
     response.status(201).json(newUser);
   } catch (error) {
     next(error);
