f9603f330a1de6facfae9aa1370b625352bd7630
norristown <jarvasity@gmail.com>
2024-05-30 19:02:47 -0400

    updated env variables

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const logger = require('./utils/logger');
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: config.url,
     methods: ['GET', 'POST'],
   },
 });
--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 
 const { PORT } = process.env;
 const { MONGODB_URI } = process.env;
+const { LOCAL, RENDER } = process.env;
+const url = process.env.NODE_ENV === 'development' ? LOCAL : RENDER;
 
 module.exports = {
   PORT,
   MONGODB_URI,
+  url,
 };
