fcf06749c1806d80b04625ce0c4fea0bc38025eb
norristown <jarvasity@gmail.com>
2024-06-24 02:13:29 -0400

    added limit for payload too large error

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose
 
 app.use(cors());
 app.use(express.static('dist'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(middleware.requestLogger);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
